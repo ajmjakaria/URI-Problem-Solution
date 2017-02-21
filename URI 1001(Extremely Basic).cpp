dd8e77a0aa249e99ee7db1291b587a9f9ba3e080
Jakaria-MU <abujafar492@gmail.com>
2017-02-22 02:08:03 +0600

    Create URI 1001(Extremely Basic).cpp

---
+++ URI 1001(Extremely Basic).cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b, X;
+
+    cin >> a >> b; //a & b contain 2 integer values
+    X = a + b;
+
+    cout << "X = " << X << endl;
+
+    return 0;
+}

aa88e52d197c6b3983e416b33cbdf05af10d3f04
Abu Jafar Md Jakaria <abujafar492@gmail.com>
2017-05-23 02:45:02 +0600

    Create URI 1044(Multiples).cpp

---
+++ URI 1044(Multiples).cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    if(a > b && a % b == 0)
+    {
+        cout << "Sao Multiplos" << endl;
+    }
+    else if(a < b && b % a == 0)
+    {
+        cout << "Sao Multiplos" << endl;
+    }
+    else
+        cout << "Nao sao Multiplos" << endl;
+    return 0;
+}
